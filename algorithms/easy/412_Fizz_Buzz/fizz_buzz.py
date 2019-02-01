26718e063933cb3294025376a77fcdf9a05fd7cc
Gary Blackwood <gary@garyblackwood.co.uk>
2019-02-01 20:59:54 +0000

    Add python3 solution for #412 Fizz Buzz

---
+++ algorithms/easy/412_Fizz_Buzz/fizz_buzz.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python
+"""
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and
+for the multiples of five output “Buzz”. For numbers which are multiples of
+both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+"""
+import unittest
+
+
+def fizzBuzz(n):
+    """
+    :type n: int
+    :rtype: List[str]
+    """
+    result = []
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    return result
+
+
+class TestSolution(unittest.TestCase):
+    def test_example(self):
+        ans = fizzBuzz(15)
+        self.assertEqual(
+            ans,
+            [
+                "1",
+                "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz",
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
