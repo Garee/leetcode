e1d7ac3bfa1805457626b98b01227edd1960f2b4
Gary Blackwood <gary@garyblackwood.co.uk>
2021-01-19 20:05:28 +0000

    Updated solution for 9. Palindrome Number

--- algorithms/easy/9_Palindrome_Number/palindrome_number.py
+++ algorithms/easy/9_Palindrome_Number/palindrome_number.py
@@ -1,39 +1,45 @@
 #! /usr/bin/env python
 """
-Determine whether an integer is a palindrome. An integer is a palindrome when
-it reads the same backward as forward.
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
 
 Example 1:
 
-    Input: 121
+    Input: x = 121
     Output: true
 
 Example 2:
 
-    Input: -121
+    Input: x = -121
     Output: false
-    Explanation: From left to right, it reads -121. From right to left, it
-    becomes 121-. Therefore it is not a palindrome.
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
 
 Example 3:
 
-    Input: 10
+    Input: x = 10
     Output: false
     Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Example 4:
+
+    Input: x = -101
+    Output: false
+
+
+Constraints:
+
+    -231 <= x <= 231 - 1
 """
 import unittest
 
 
-def is_palindrome(x):
+def is_palindrome(x: int) -> bool:
     """
-    :type x: int
-    :rtype: bool
+    O(1) time and space.
     """
-    if x < 0:
-        return False
     x_str = str(x)
-    x_rev_str = x_str[::-1]
-    return x_str == x_rev_str
+    return x_str == x_str[::-1]
 
 
 class TestSolution(unittest.TestCase):
